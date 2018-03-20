6ec8fe292093aca630c026f4cdea7bb13eaaa9c3
William M <wmooney1984@gmail.com>
2018-03-20 19:41:22 -0400

    Front-End Libraries - Random Quote Generator - Random Functionality Added

--- FrontEndLibraries/Projects/RandomQuoteGen/app.js
+++ FrontEndLibraries/Projects/RandomQuoteGen/app.js
@@ -1,20 +1,28 @@
-// JavaScript source code
+// Bible Quote Generator
 
 
 const quoteDB =
     [{
-        author: "", 
-        quote: ""
+        author: "John 14:6", 
+        quote: "Yeshua said, 'I AM the Way — and the Truth and the Life; no one comes to the Father except through me.'"
     }, {
-        author: "",
-        quote: ""
+        author: "Jeremiah 29:11",
+        quote: "'For I know what plans I have in mind for you,' says Adonai,'plans for well-being, not for bad things; so that you can have hope and a future.'"
     }, {
-        author: "",
-        quote: ""
+        author: "John 3:16",
+        quote: "'For God so loved the world that he gave his only and unique Son, so that everyone who trusts in him may have eternal life, instead of being utterly destroyed.'"
     }, {
-        author: "",
-        quote: ""
+        author: "James 4:7",
+        quote: "Therefore, submit to God. Moreover, take a stand against the Adversary, and he will flee from you."
     }, {
-        author: "",
-        quote: ""
+        author: "Mark 12:30-31",
+        quote: "'You are to love Adonai your God with all your heart, with all your soul, with all your understanding and with all your strength.' The second is this: 'You are to love your neighbor as yourself.' There is no other commandment greater than these."
     }];
+
+const randomQuote = () => {
+    let rand = Math.floor(Math.random() * 5);
+
+    return quoteDB[rand];
+}
+
+console.log(randomQuote());
