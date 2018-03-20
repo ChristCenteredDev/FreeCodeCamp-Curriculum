e8d29ec14d22c8e69501206836936f982e870a9e
William M <wmooney1984@gmail.com>
2018-03-20 19:15:37 -0400

    Front-End Libraries - Random Quote Generator - Initial Commit

---
+++ FrontEndLibraries/Projects/RandomQuoteGen/app.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+
+
+const quoteDB =
+    [{
+        author: "", 
+        quote: ""
+    }, {
+        author: "",
+        quote: ""
+    }, {
+        author: "",
+        quote: ""
+    }, {
+        author: "",
+        quote: ""
+    }, {
+        author: "",
+        quote: ""
+    }];
