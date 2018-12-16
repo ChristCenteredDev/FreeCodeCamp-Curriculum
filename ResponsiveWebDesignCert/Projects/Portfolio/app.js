3386283ddb0e735192088526a3e9c64d3590dfa1
William M <wmooney1984@gmail.com>
2018-12-15 20:23:31 -0500

    Portfolio - Bugfix: Tabs

--- ResponsiveWebDesignCert/Projects/Portfolio/app.js
+++ ResponsiveWebDesignCert/Projects/Portfolio/app.js
@@ -1,5 +1,5 @@
 // Starting with jQuery - Watched the video's
 
 $(document).ready(function () {  // Only runs the JS code once the webpage fully loaded
-    /*  <<< Insert all JS here >>>  */
+  /*  <<< Insert all JS here >>>  */
 });
